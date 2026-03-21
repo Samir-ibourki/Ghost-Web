846451e2f450b14222282868b547fd9edd962703
Samir-ibourki <samiribourki7@gmail.com>
2026-03-21 10:30:23 +0000

    init of projet ghost energy

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <section className="bg-gray-400 h-screen">
+      <Header />
+    </section>
+  );
+}
+
+export default App;
